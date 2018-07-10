dd0fb77e2ac75c15eb2e184741fbb09ebc69714e
Terry Teng <terry.teng@oocl.com>
2018-07-10 13:30:11 +0800

    [Terry] practice 02 finished

--- src/main/java/practice02/Student.java
+++ src/main/java/practice02/Student.java
@@ -1,6 +1,18 @@
 package practice02;
 
-import Person.practice02;
 
 public class Student extends Person {
+    private int klass;
+    public Student(String _name, int _age, int _klass) {
+        super(_name, _age);
+        klass = _klass;
+    }
+
+    public String introduce() {
+        return String.format("I am a Student. I am at Class %d.", klass);
+    }
+
+    public int getKlass() {
+        return klass;
+    }
 }
