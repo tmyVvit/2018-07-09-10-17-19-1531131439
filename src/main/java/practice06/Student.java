250c79dc041ceba39d912836afebacc78d1e8f8d
Terry Teng <terry.teng@oocl.com>
2018-07-10 13:55:51 +0800

    [Terry] practice 06 finished

--- src/main/java/practice06/Student.java
+++ src/main/java/practice06/Student.java
@@ -1,4 +1,21 @@
 package practice06;
 
-public class Student {
+public class Student extends Person {
+    private int klass;
+
+    public Student(String _name, int _age){
+        super(_name, _age);
+    }
+    public Student(String _name, int _age, int _klass) {
+        super(_name, _age);
+        klass = _klass;
+    }
+
+    public String introduce() {
+        return String.format("%s I am a Student. I am at Class %d.", super.introduce(), klass);
+    }
+
+    public int getKlass() {
+        return klass;
+    }
 }
--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,25 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person{
+    private int klass;
+    public Teacher(String _name, int _age) {
+        super(_name, _age);
+    }
+    public Teacher(String _name, int _age, int _klass) {
+        super(_name, _age);
+        klass = _klass;
+    }
+
+    public int getKlass(){
+        return klass;
+    }
+
+    public String introduce() {
+        String teachOrNot = "No Class";
+        if(klass != 0){
+            teachOrNot = String.format("Class %d", klass);
+        }
+        return String.format("%s I am a Teacher. I teach %s.", super.introduce(), teachOrNot);
+    }
+
 }
