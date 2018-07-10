b5b41224c16959cda08cb55bb61ce5cfbb0d4fb0
Terry Teng <terry.teng@oocl.com>
2018-07-10 14:36:39 +0800

    [Terry] practice 08 finished

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,29 @@
 package practice08;
 
 public class Klass {
+    private int number;
+    private Student leader;
+    public Klass(int _number) {
+        number = _number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public Student getLeader() {return leader;}
+
+    public boolean equals(Klass _klass) {
+        return number==_klass.getNumber();
+    }
+
+    public String getDisplayName(){
+        return String.format("Class %d", number);
+    }
+
+    public void assignLeader(Student student) {
+        if(equals(student.getKlass())){
+            leader = student;
+        }
+    }
 }
