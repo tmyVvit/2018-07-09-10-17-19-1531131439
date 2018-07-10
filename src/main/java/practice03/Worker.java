5d19cfb08c09b6b48484eec186f55357f86faeeb
Terry Teng <terry.teng@oocl.com>
2018-07-10 13:33:58 +0800

    [Terry] practice 03 finished

--- src/main/java/practice03/Worker.java
+++ src/main/java/practice03/Worker.java
@@ -1,4 +1,13 @@
 package practice03;
 
-public class Worker {
+public class Worker extends Person{
+    private String name;
+    private int age;
+    public Worker(String _name, int _age){
+        super(_name, _age);
+    }
+
+    public String introduce() {
+        return "I am a Worker. I have a job.";
+    }
 }
